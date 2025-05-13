43102130408fae52b109b684da78353bdd21ca08
ssllcc <161134864+ssllcc@users.noreply.github.com>
2025-05-13 23:13:18 +0800

    Update send_commands.py

--- send_commands.py
+++ send_commands.py
@@ -33,3 +33,27 @@ def send_youtube_command(mac_user, mac_ip):
         print("Direct YouTube command sent successfully.")
     except subprocess.CalledProcessError as e:
         print("Failed to send Direct YouTube command.", e)
+
+def send_volume_up_command(mac_user, mac_ip):
+    ssh_command = (
+        f'ssh {mac_user}@{mac_ip} '
+        # '"osascript -e \'tell application \\"Notes\\" to activate\' -e \'tell application \\"System Events\\" to keystroke \\"v\\" using {command down}\'"'
+        '"osascript -e \'tell applicatoin \\"Notes\\" to activate\' -e \'tell application \\"System Events\\" to set volume output volume (output volume of (get volume settings) - 5)"'
+    )
+    try:
+        subprocess.run(ssh_command, shell=True, check=True)
+        print("Volume up command sent successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Failed to send volume up command.", e)
+
+def send_volume_down_command(mac_user, mac_ip):
+    ssh_command = (
+        f'ssh {mac_user}@{mac_ip} '
+        # '"osascript -e \'tell application \\"Notes\\" to activate\' -e \'tell application \\"System Events\\" to keystroke \\"v\\" using {command down}\'"'
+        '"osascript -e \'tell applicatoin \\"Notes\\" to activate\' -e \'tell application \\"System Events\\" to set volume output volume (output volume of (get volume settings) + 5)"'
+    )
+    try:
+        subprocess.run(ssh_command, shell=True, check=True)
+        print("Volume down command sent successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Failed to send volume down command.", e)
